5fc4c837f6e1134f0900e24e5e12815229240c60
Luis Román <romnluis@yahoo.com>
2018-04-25 12:38:43 -0500

    se agregó el login vinculado a mysql y una ventana de Menu

--- ventas/ventas/MainForm.cs
+++ ventas/ventas/MainForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace ventas
 {
@@ -17,8 +18,29 @@ namespace ventas
 		void BtnAceptarClick(object sender, EventArgs e)
 		{
 			//accción tempotal para probar conexion
-			conexion.ObtenerConexion();
-			MessageBox.Show("conectado!");
+			//conexion.ObtenerConexion();
+			
+			MySqlCommand codigo = new MySqlCommand();
+			
+			MySqlConnection cn = new MySqlConnection();
+			
+			codigo.Connection = conexion.ObtenerConexion();
+			
+			codigo.CommandText = ("select * from usuarios where NICKNAME = '"+txtUsuario.Text+"' and PASSWORD = '"+txtContraseña.Text+"' ");
+			
+			MySqlDataReader leer = codigo.ExecuteReader();
+			
+			if(leer.Read()){
+				MessageBox.Show("Bienvenido al sistema "+txtUsuario.Text+"!");
+				Menu m =  new Menu();
+				m.Show();
+				this.Hide();
+				
+			}
+			else{
+				MessageBox.Show("Usuario o contraseña incorrectos!");
+			}
+			
 			
 		}
 		
--- ventas/ventas/Menu.cs
+++ ventas/ventas/Menu.cs
@@ -1,32 +1,17 @@
-﻿/*
- * Created by SharpDevelop.
- * User: Luis
- * Date: 24/04/2018
- * Time: 11:02 p.m.
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
-using System;
+﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
 
 namespace ventas
 {
-	/// <summary>
-	/// Description of Menu.
-	/// </summary>
+
 	public partial class Menu : Form
 	{
 		public Menu()
 		{
-			//
-			// The InitializeComponent() call is required for Windows Forms designer support.
-			//
+
 			InitializeComponent();
-			
-			//
-			// TODO: Add constructor code after the InitializeComponent() call.
-			//
+
 		}
 	}
 }
