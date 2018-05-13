b025a274f87659059352550516ac2517125d68ac
Luis Román <romnluis@yahoo.com>
2018-05-13 01:18:19 -0500

    Cambios de deltoro :v
    Creación y modificación de nuevas ventanas integradas al sistema.

---
+++ ventas/ventas/ModificarUsuario.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: JL
+ * Fecha: 12/05/2018
+ * Hora: 09:44 p.m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ventas
+{
+	/// <summary>
+	/// Description of ModificarUsuario.
+	/// </summary>
+	public partial class ModificarUsuario : Form
+	{
+		public ModificarUsuario()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
