b55305b43599073105cf5aa11b7cec239184e7a8
Luis Román <romnluis@yahoo.com>
2018-05-15 11:16:15 -0500

    actualización

--- ventas/ventas/Clases/conexion.cs
+++ ventas/ventas/Clases/conexion.cs
@@ -12,7 +12,7 @@ namespace ventas
        {
 		 		// cambiar por el usuario y contraseña de la db que se tenga.
 		 		MySqlConnection conectar = new MySqlConnection(
-		 			"server=127.0.0.1; database=p_venta1; Uid=root; pwd=1234;");
+		 			"server=127.0.0.1; database=p_venta1; Uid=root; pwd=newborn;");
 								//newborn
            		conectar.Open();
            		return conectar;
